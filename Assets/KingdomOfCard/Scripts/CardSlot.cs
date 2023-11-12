1a558dc2cd7831aa5b6b0b14b38a7f9e16267a6d
nan <huizhouwunan@hotmail.com>
2023-11-11 21:53:26 -0800

    multiplayers working!!!!!!!
    able to see the changes other player made.

--- Assets/KingdomOfCard/Scripts/CardSlot.cs
+++ Assets/KingdomOfCard/Scripts/CardSlot.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Foundry.Networking;
 
-public class CardSlot : MonoBehaviour
+public class CardSlot : NetworkComponent
 {
     public GameObject Monster;
     public Material Neutral;
     public Material Selected;
     public MeshRenderer meshRenderer;
+    private NetworkProperty<bool> _spawn = new(false);
+
+    public bool spawn
+    {
+        get => _spawn.Value;
+        set => _spawn.Value = value;
+    }
+
 
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    /* RegisterProperties is called once when the component is added to the networked object on Awake, 
+     * this is where we connect up all our properties.*/
+    public override void RegisterProperties(List<INetworkProperty> props)
+    {
+        // This callback is called both when the value is set locally and when it is set remotely.
+        _spawn.OnValueChanged += b =>
+        {
+            Monster.SetActive(b);
+            if (b)
+            {
+                meshRenderer.material = Selected;
+            }
+            else
+            {
+                meshRenderer.material = Neutral;
+            }
+        };
+        props.Add(_spawn);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -21,9 +50,12 @@ public class CardSlot : MonoBehaviour
 
         if (other.name.ToLower().Contains("finger"))
         {
+            // Change network'ed spawn value
+            spawn = !spawn;
+
             //spawn monster
-            Invoke("ShowMonster", 1f);
-            meshRenderer.material = Selected;
+            // Invoke("ShowMonster", 1f);
+            // meshRenderer.material = Selected;
         }
     }
 
