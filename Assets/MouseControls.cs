c9a68a4ec84a27f80d822d6b37de5848a86f9245
azb <azbaney@gmail.com>
2023-11-15 01:24:52 -0800

    Cleanup mouse controls script

--- Assets/MouseControls.cs
+++ Assets/MouseControls.cs
@@ -8,18 +8,11 @@ public class MouseControls : MonoBehaviour
     {
         if (!XRSettings.isDeviceActive)
         {
-            // Check if the left mouse button is clicked
-            Debug.Log("dsfdsgds");
-            // Cast a ray from the mouse position
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            // Check if the ray hits any collider
             if (Physics.Raycast(ray, out hit))
             {
-                // Log the name of the object hit
-                Debug.Log("Hit object: " + hit.collider.gameObject.name);
-
                 if (hit.collider.gameObject.name.Contains("CardSlot")
                     && Input.GetMouseButtonDown(0))
                 {
@@ -33,9 +26,6 @@ public class MouseControls : MonoBehaviour
                     InteractableUnityEventWrapper button = hit.collider.gameObject.GetComponentInParent< InteractableUnityEventWrapper >();
                     button.WhenUnselect.Invoke();
                 }
-
-                // You can perform other actions with the hit object here
-                // For example, you can access hit.collider.gameObject to get the GameObject that was hit
             }
         }
     }
