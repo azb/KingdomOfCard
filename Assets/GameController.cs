da3a2d5b5d331f93dbf6fff145087e71a9b61ab3
azb <azbaney@gmail.com>
2023-11-16 00:26:37 -0800

    Make attacks do damage, update healthbars

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -2,6 +2,59 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public float Player1Health
+    {
+        get
+        {
+            return networkedObject.GetSyncedFloat("Player1Health");
+        }
+        set
+        {
+            networkedObject.SetSyncedFloat("Player1Health", value);
+        }
+    }
+    public float Player2Health
+    {
+        get
+        {
+            return networkedObject.GetSyncedFloat("Player2Health");
+        }
+        set
+        {
+            networkedObject.SetSyncedFloat("Player2Health", value);
+        }
+    }
+
+
+    public float Player1Mana
+    {
+        get
+        {
+            return networkedObject.GetSyncedFloat("Player1Mana");
+        }
+        set
+        {
+            networkedObject.SetSyncedFloat("Player1Mana", value);
+        }
+    }
+
+    public float Player2Mana
+    {
+        get
+        {
+            return networkedObject.GetSyncedFloat("Player2Mana");
+        }
+        set
+        {
+            networkedObject.SetSyncedFloat("Player2Mana", value);
+        }
+    }
+
+    public Transform Player1HealthBar;
+    public Transform Player2HealthBar;
+    public Transform Player1ManaBar;
+    public Transform Player2ManaBar;
+
     public enum Turn
     {
         Player1sTurn = 0,
@@ -48,10 +101,42 @@ public class GameController : MonoBehaviour
 
     public static GameController Instance;
 
+    Vector3 StartBarScale;
+
     private void Start()
     {
+        Player1Health = 100;
+        Player2Health = 100;
+        Player1Mana = 100;
+        Player2Mana = 100;
+
         Instance = this;
         InvokeRepeating("UpdateButtonColors", .5f, .5f);
+
+        StartBarScale = Player1HealthBar.localScale;
+    }
+
+    private void Update()
+    {
+        Player1HealthBar.localScale = Vector3.Scale(
+            StartBarScale,
+            new Vector3(Player1Health / 100f, 1, 1)
+        );
+
+        Player2HealthBar.localScale = Vector3.Scale(
+            StartBarScale,
+            new Vector3(Player2Health / 100f, 1, 1)
+        );
+
+        Player1ManaBar.localScale = Vector3.Scale(
+            StartBarScale,
+            new Vector3(Player1Mana / 100f, 1, 1)
+        );
+
+        Player2ManaBar.localScale = Vector3.Scale(
+            StartBarScale,
+            new Vector3(Player2Mana / 100f, 1, 1)
+        );
     }
 
     void UpdateButtonColors()
@@ -72,7 +157,7 @@ public class GameController : MonoBehaviour
         else
         {
             Instance.Player2ButtonRenderer.material = Instance.ButtonDisabledMaterial;
-        }        
+        }
     }
 
     public static void Player1NextTurnButtonPressed()
@@ -96,10 +181,12 @@ public class GameController : MonoBehaviour
     void Player1FinishedAttacking()
     {
         turn = Turn.Player2sTurn;
+        Player2Health -= 10;
     }
 
     void Player2FinishedAttacking()
     {
         turn = Turn.Player1sTurn;
+        Player1Health -= 10;
     }
 }
