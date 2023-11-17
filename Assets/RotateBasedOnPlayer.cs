cde86aeff3bfa1471f2bafcce5f1047607d3158a
azb <azbaney@gmail.com>
2023-11-16 16:55:52 -0800

    Update RotateBasedOnPlayer.cs

--- Assets/RotateBasedOnPlayer.cs
+++ Assets/RotateBasedOnPlayer.cs
@@ -8,20 +8,21 @@ public class RotateBasedOnPlayer : MonoBehaviourPunCallbacks
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
 
     public override void OnJoinedRoom()
     {
-        if (!PhotonNetwork.IsMasterClient)
+        Debug.Log("Joined Room Actor Number = " + PhotonNetwork.LocalPlayer.ActorNumber);
+        if (PhotonNetwork.LocalPlayer.ActorNumber == 1)
         {
-            Debug.Log("Is not master so rotating");
-            transform.localRotation = Quaternion.Euler(0,180,0);
+            Debug.Log("Is master so not rotating");
         }
         else
         {
-            Debug.Log("Is master so not rotating");
+            Debug.Log("Is not master so rotating");
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
         }
     }
 
